2b69b62afcc30909f77713285ced9f5afe23daa2
jimbib <joniemartens@icloud.com>
2024-01-18 14:27:43 +0100

    combined repeated logic,

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// API Link
+const RandomFact = 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=de';
+
+// Game-related variables
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const controls = document.querySelectorAll(".controls i");
@@ -17,19 +21,14 @@ let delay = 100;
 let rows = 30;
 let columns = 30;
 
-// API Link
-const RandomFact = 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=de';
-
-
-
-// Create a playboard
+// Function to create the playboard
 const createPlayBoard = () => {
     playBoard.innerHTML = "";
     playBoard.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     playBoard.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
 };
 
-// Reduce playboardsize by 1 Column and Row
+// Function to reduce playboard size
 const reducePlayBoardSize = () => {
     if (rows > 1 && columns > 1) {
         rows -= 1;
@@ -39,59 +38,55 @@ const reducePlayBoardSize = () => {
     }
 };
 
-// Increase speed by 10 
+// Function to increase delay and speed
 const increaseDelay = () => {
     delay -= 10;
     clearInterval(snakeInterval);
-    snakeInterval = setInterval(initGame, delay);
-}
+    snakeInterval = setInterval(startGame, delay);
+};
 
-// Decrease speed by 20 and add 50 speed
+// Function to level up and reset foodEaten
 const levelUp = () => {
     clearInterval(snakeInterval);
-    //    alert(`Level Up`);
     foodEaten = 0;
-    delay = Math.max(delay - 20, 50);  // speed for the next level
-    snakeInterval = setInterval(initGame, delay);
-}
+    delay = Math.max(delay - 20, 50);
+    snakeInterval = setInterval(startGame, delay);
+};
 
-// update the foodposition
+// Function to update the food position
 const updateFoodPosition = () => {
-    // Passing a random 1 - 30 value as food position
     foodX = Math.floor(Math.random() * rows) + 1;
     foodY = Math.floor(Math.random() * columns) + 1;
-}
+};
 
-// Call API
+// Async function to call API and display fact on game over
 async function userAction() {
-    const response = await fetch(RandomFact, {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json"
+    try {
+        const response = await fetch(RandomFact);
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
         }
-    });
-    const myJson = await response.json(); //extract JSON from the http response
 
-    const parsejson = JSON.stringify(myJson, ["text"]); // parse Json into string
-    console.log(parsejson);
-    alert("Schade ... Game Over, aber wusstest du schon: " + myJson["text"]);
+        const myJson = await response.json();
+        const parsejson = JSON.stringify(myJson, ["text"]);
+        console.log(parsejson);
+        alert("Schade ... Game Over, aber wusstest du schon: " + myJson["text"]);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
-// Game Over Message
-async function handleGameOver() {
-    // Clearing the timer and reloading the page on game over
-    clearInterval(setIntervalId);
-    // Game Over
-    // alert(userAction());
+// Function to handle game over and display highscores
+const handleGameOver = async () => {
+    clearInterval(snakeInterval);
     await userAction();
     location.reload();
     displayHighscores();
-}
+};
 
-// Change Direction of the Snake
+// Function to change direction based on keyboard input
 const changeDirection = e => {
-    // Changing velocity value based on key press
-    if (e.key === "ArrowUp" && velocityY != 1) {
+    if (e.key === "ArrowUp" && velocityY !== 1) {
         velocityX = 0;
         velocityY = -1;
     } else if (e.key === "ArrowDown" && velocityY !== -1) {
@@ -104,9 +99,9 @@ const changeDirection = e => {
         velocityX = 1;
         velocityY = 0;
     }
-}
+};
 
-// Timer to eat food
+// Function to start the timer
 const startTimer = () => {
     const timerSpan = document.getElementById("timerSpan");
 
@@ -120,32 +115,31 @@ const startTimer = () => {
         }
     };
 
-
-    // Initial call und interval for timer
     updateTimer();
     const timerInterval = setInterval(updateTimer, 1000);
 
     return timerInterval;
 };
 
+// Function to stop the timer
 const stopTimer = () => {
     clearInterval(timerInterval);
 };
 
-// Load Highscore from localStorage
+// Load highscore from localStorage
 let highscores = JSON.parse(localStorage.getItem('highscores')) || [];
 
-// Save Highscore in localStorage
+// Function to save highscore in localStorage
 const saveHighscore = (username, score) => {
     highscores.push({ username, score });
-    highscores = highscores.sort((a, b) => b.score - a.score).slice(0, 10); // Sortieren und auf 10 Einträge begrenzen
+    highscores = highscores.sort((a, b) => b.score - a.score).slice(0, 10);
     localStorage.setItem('highscores', JSON.stringify(highscores));
 };
 
+// Function to display highscores in the table
 const displayHighscores = () => {
     const highscoresBody = document.getElementById("highscoresBody");
 
-    // Fill the Highscore table
     highscores.forEach((entry, index) => {
         const row = document.createElement("tr");
         row.innerHTML = `<td>${index + 1}</td><td>${entry.username}</td><td>${entry.score}</td>`;
@@ -153,10 +147,10 @@ const displayHighscores = () => {
     });
 };
 
-// Calling changeDirection on each key click and passing key dataset value as an object
+// Event listeners for button clicks and keyboard input
 controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
-// Initialise Game
+// Function to initialize the game
 const startGame = () => {
     const currentTime = Date.now();
     const timeElapsed = currentTime - lastMoveTime;
@@ -168,38 +162,33 @@ const startGame = () => {
             handleGameOver();
             gameOver = false;
             const username = prompt("Game over! Enter your username:");
-        if (username) {
-            saveHighscore(username, score);
-            displayHighscores();
+            if (username) {
+                saveHighscore(username, score);
+                displayHighscores();
+            }
+            return;
         }
-            return
-        }
-        
+
         let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
-        // Checking if the snake hit the food
         if (snakeX === foodX && snakeY === foodY) {
             updateFoodPosition();
             snakeBody.push([foodY, foodX]);
             scoreElement.innerText = `Score: ${score += 1}`;
             foodEaten += 1;
 
-            // Increase speed after eating 5 food
             if (foodEaten % 5 === 0) {
                 increaseDelay();
             }
 
-            // Level up after eating 10 food
             if (foodEaten % 10 === 0) {
                 levelUp();
             }
 
-            // Reduce Playboardsize after eating 5 food
             if (foodEaten % 5 === 0) {
                 reducePlayBoardSize();
             }
 
-            // Reset the timer when a fruit is collected
             clearInterval(snakeInterval);
             timer = 20;
             document.getElementById("timerSpan").innerText = timer;
@@ -207,40 +196,30 @@ const startGame = () => {
             snakeInterval = startTimer();
         }
 
-        // Updating the snake's head position based on the current velocity
         snakeX += velocityX;
         snakeY += velocityY;
 
-        // Shifting forward the values of the elements in the snake body by one
         for (let i = snakeBody.length - 1; i > 0; i--) {
             snakeBody[i] = snakeBody[i - 1];
         }
-        snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
+        snakeBody[0] = [snakeX, snakeY];
 
-        // Checking if the snake's head is out of wall, if so setting gameOver to true
         if (snakeX <= 0 || snakeX > rows || snakeY <= 0 || snakeY > columns) {
             return gameOver = true;
         }
 
         for (let i = 0; i < snakeBody.length; i++) {
-            // Adding a div for each part of the snake's body
-            if (i === 0) {
-                // Add the orange-head class to the head of the snake
-                html += `<div class="head orange-head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-            } else {
-                html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-            }
+            const isHead = i === 0;
+            const className = isHead ? "head orange-head" : "head";
+            html += `<div class="${className}" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
 
-            // Checking if the snake head hit the body, if so set gameOver to true
             if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
                 gameOver = true;
             }
         }
 
         for (let i = 0; i < snakeBody.length - snakeBody.length + 1; i++) {
-            // Adding a div for each part of the snake's body
             html += `<div class="tail" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-            // Checking if the snake head hit the body, if so set gameOver to true
             if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
                 gameOver = true;
             }
@@ -250,7 +229,7 @@ const startGame = () => {
     }
 };
 
+// Initial setup
 updateFoodPosition();
 snakeInterval = setInterval(startGame, 100);
-//snakeInterval = startTimer();
 document.addEventListener("keyup", changeDirection);
